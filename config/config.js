4dc803c0065f3750cd3be442c97fd8cf327a137c
Latifah Lillie <queen1892@gmail.com>
2021-03-14 19:42:04 -0500

    Updating the Main Page with a NavBar

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = 
+{
+    "development": {
+        "username": process.env.MYSQL_USER,
+        "password": process.env.MYSQL_PW,
+        "database": process.env.MYSQL_DB,
+        "host": process.env.MYSQL_HOST,
+        "port": 3306,
+        "dialect": "mysql"
+    },
+    "test": {
+        "username": "root",
+        "password": null,
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    },
+    "production": {
+        "use_env_variable": "JAWSDB_URL",
+        "username": "root",
+        "password": null,
+        "database": "database_production",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    }
+}
